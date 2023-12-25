83a34942de25f37764a537a7695b5d87378c63d1
Suratej Ponnapalli <suryatej.ind@gmail.com>
2023-12-25 15:15:09 +0530

    second push

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -17,7 +17,7 @@ const BlogDetails = () => {
 
     return ( 
         <div className="blog-details">
-            { isPending && <div>Loading...</div> }
+            { isPending && <div>Loading....</div> }
             { blog && (
                 <article>
                     <h2>{ blog.title }</h2>
